06c56d69310163acda47458a298528b346a3bee1
TuanLe <tuan.le@exe.com.vn>
2016-08-08 16:54:26 +0700

    Support moduleConfig

--- src/CMS/Foundation/Mvc/Application.php
+++ src/CMS/Foundation/Mvc/Application.php
@@ -1,18 +1,27 @@
 <?php
 namespace CMS\Foundation\Mvc;
 
+use CMS\Foundation\Module\Module;
+
 class Application extends \Phalcon\Mvc\Application
 {
+    /**
+     * @return Module
+     */
+    public function module()
+    {
+        return $this->module;
+    }
+
+    /**
+     * @param array $modules
+     * @param bool  $merge
+     *
+     * @return void
+     */
     public function registerModules(array $modules, $merge = false)
     {
         parent::registerModules($modules, $merge);
-
-        /**
-         * Register module
-         */
-        foreach ($modules as $name => $module) {
-            
-        }
     }
 
 }
