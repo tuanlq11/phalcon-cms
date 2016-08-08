0e91f4cbaa7eb1631ce5303c67a6fa1e19b2f258
TuanLe <tuan.le@exe.com.vn>
2016-08-08 16:55:40 +0700

    Support moduleConfig

--- src/CMS/Foundation/Mvc/Application.php
+++ src/CMS/Foundation/Mvc/Application.php
@@ -1,18 +1,8 @@
 <?php
 namespace CMS\Foundation\Mvc;
 
-use CMS\Foundation\Module\Module;
-
 class Application extends \Phalcon\Mvc\Application
 {
-    /**
-     * @return Module
-     */
-    public function module()
-    {
-        return $this->module;
-    }
-
     /**
      * @param array $modules
      * @param bool  $merge
--- src/CMS/Foundation/Mvc/Controller.php
+++ src/CMS/Foundation/Mvc/Controller.php
@@ -1,6 +1,15 @@
 <?php
 namespace CMS\Foundation\Mvc;
 
+use CMS\Foundation\Module\Module;
+
 class Controller extends \Phalcon\Mvc\Controller
 {
+    /**
+     * @return Module
+     */
+    public function module()
+    {
+        return $this->module;
+    }
 }
