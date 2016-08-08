4b3af78b7a03f8dc8303bcfd4bed06b5ec918341
TuanLe <tuan.le@exe.com.vn>
2016-08-08 14:34:55 +0700

    Support 404Handle

--- src/CMS/Plugin/Exception.php
+++ src/CMS/Plugin/Exception.php
@@ -8,6 +8,12 @@ class Exception
 {
     public function beforeException(Event $event, Dispatcher $dispatcher, $exception)
     {
+        $dispatcher->forward([
+            "module"     => app()::KERNEL_SKELETON,
+            "controller" => "error",
+            "action"     => "default",
+        ]);
 
+        return false;
     }
 }
