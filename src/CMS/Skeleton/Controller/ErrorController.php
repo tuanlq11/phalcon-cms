a28a175f5d6b46e01593f1433b25bc6218fe62fe
TuanLe <tuan.le@exe.com.vn>
2016-08-09 11:07:00 +0700

    Support base translation

--- src/CMS/Skeleton/Controller/ErrorController.php
+++ src/CMS/Skeleton/Controller/ErrorController.php
@@ -5,9 +5,16 @@ use CMS\Foundation\Mvc\Controller;
 
 class ErrorController extends Controller
 {
+    /**
+     * @param $exception \Exception
+     */
     public function defaultAction($exception)
     {
         $this->view->setViewsDir(__DIR__ . "/../View");
-        $this->view->exception = $exception;
+        $this->view->message = $exception->getMessage();
+        $this->view->code    = $exception->getCode();
+        $this->view->file    = $exception->getFile();
+        $this->view->line    = $exception->getLine();
+        $this->view->trace   = $exception->getTrace();
     }
 }
