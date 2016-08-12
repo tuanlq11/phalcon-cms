3ef6b8318c4720bd70f41ec1e31f3f170eeb2f47
TuanLe <tuan.le@exe.com.vn>
2016-08-12 15:34:28 +0700

    Support regenerate id

--- src/CMS/Foundation/Session/Adapter/Redis.php
+++ src/CMS/Foundation/Session/Adapter/Redis.php
@@ -1,8 +1,52 @@
 <?php
 namespace CMS\Foundation\Session\Adapter;
 
+/**
+ * Class Redis
+ *
+ * @package CMS\Foundation\Session\Adapter
+ */
 class Redis extends \Phalcon\Session\Adapter\Redis
 {
+    /** @var \Phalcon\Cache\Backend\Redis */
+    protected $_redis = null;
+
+    /**
+     * Redis constructor.
+     *
+     * @param array $options
+     *
+     */
+    public function __construct(array $options = [])
+    {
+        if (!isset($options["host"])) {
+            $options["host"] = "127.0.0.1";
+        }
+
+        if (!isset($options["port"])) {
+            $options["port"] = 6379;
+        }
+
+        if (!isset($options["persistent"])) {
+            $options["persistent"] = false;
+        }
+
+        $this->_lifetime = array_get($options, "lifetime", $this->_lifetime);
+
+        session_set_save_handler(
+            [$this, "open"],
+            [$this, "close"],
+            [$this, "read"],
+            [$this, "write"],
+            [$this, "destroy"],
+            [$this, "gc"],
+            [$this, "createId"]
+        );
+
+        parent::__construct($options);
+    }
+
+
     /**
      * @param bool $deleteOldSession
      *
@@ -10,7 +54,9 @@ class Redis extends \Phalcon\Session\Adapter\Redis
      */
     public function regenerateId($deleteOldSession = true)
     {
+        $oldSession = $this->read($this->getId());
         $this->_redis->delete($this->getId());
+
         setcookie($this->getName(), "", time() - 86400, "/");
 
         return $this;
